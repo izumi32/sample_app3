d1d5b4996110cc982837c07c687950a7eeaee967
izumi32 <k.izumi721@gmail.com>
2020-12-26 18:24:03 +0900

    ch11実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      flash[:success] = "ログインしました"
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        flash[:success] = "ログインしました"
+        redirect_back_or user
+      else
+        message = "アカウントが有効化されていません"
+        message += "メールを確認してください"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = '入力情報に間違いがあります'
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "アカウントを作成しました"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "アカウントを有効化するメールを送信しました"
+      redirect_to root_url
     else
       render 'new'
     end
