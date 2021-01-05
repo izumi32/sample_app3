8be99cd727ac727f532a338d297afcd1e5a9b832
izumi32 <k.izumi721@gmail.com>
2021-01-05 19:50:04 +0900

    ch12実装

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-  mail       to: user.email, 
-        subject: "アカウント有効化"
+    mail to: user.email, subject: "アカウント有効化"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "パスワード再設定"
   end
 end
