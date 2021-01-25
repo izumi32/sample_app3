070076b064df6f49138396d6c033ffb146cff21f
izumi32 <k.izumi721@gmail.com>
2021-01-25 18:23:22 +0900

    ch14実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships,
+                        source: :followed
+  has_many :followers, through: :passive_relationships,
+                        source: :follower
   attr_accessor :remember_token,
                 :activation_token,
                 :reset_token
@@ -79,7 +89,22 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", self.id)
+    following_ids = "SELECT followed_id FROM relationships
+                    WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                    OR user_id = :user_id", user_id: id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
@@ -92,6 +117,5 @@ class User < ApplicationRecord
   def downcase_email
     self.email = self.email.downcase
   end
-
 
 end
